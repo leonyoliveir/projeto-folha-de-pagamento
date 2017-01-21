905bf125c8f22a20d469e4c597128366a8309280
leonyoliveir <leony.oliveira.bernardo@hotmail.com>
2017-01-21 12:53:41 -0200

    Delete Empregado.java

--- Empregado.java
+++
@@ -1,9 +0,0 @@
-
-public class Empregado {
-	
-	public String nome, endereco, metodo;
-	public int numeroCadastro;
-	public char tipoPagamento;
-	public double salarioHora, salarioFixo, horas, comissao, percentual;
-	
-}
